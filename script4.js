bde6c95ac9000e6963c7a43a35532f328e8732c0
mingman <ingman.m@gmail.com>
2024-01-11 13:16:22 +0100

    second half of assignment

--- script4.js
+++ script4.js
@@ -1,10 +1,19 @@
-// --- ENKEL DATAREGISTRERING ---
+// // --- ENKEL DATAREGISTRERING ---
 
-let name = prompt("Vad heter du?");
-console.log("Välkommen " + name + "!Jalma");
-let birthyear = prompt("vilket år är du född?");
-let currentDate = new Date();
-console.log(
-  "Iår kommer du att fylla eller har redan fyllt " +
-    (currentDate.getFullYear() - birthyear)
-);
+// let name = prompt("Vad heter du?");
+// console.log("Välkommen " + name + "!Jalma");
+// let birthyear = prompt("vilket år är du född?");
+// let currentDate = new Date();
+// console.log(
+//   "Iår kommer du att fylla eller har redan fyllt " +
+//     (currentDate.getFullYear() - birthyear)
+// );
+let num1 = prompt("Ange första numret");
+let num2 = prompt("Ange andra numret");
+
+console.log(parseInt(num1) + parseInt(num2));
+console.log(parseInt(num1) - parseInt(num2));
+console.log(parseInt(num1) * parseInt(num2));
+console.log(parseInt(num1) / parseInt(num2));
+
+alert("Beräkningarna är klara. Se konsoll för resultat");
