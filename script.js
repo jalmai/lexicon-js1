3756312fe9c7fed1c9fad4e8d7289ff37a9037c5
mingman <ingman.m@gmail.com>
2024-01-11 09:25:56 +0100

    1.3

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // let age = 32;
 // console.log("my age is " + age);
-var hej = "VAR can have local or global scope";
-let hello =
-  "LET can have local, global or block scope. Let is preferred over VAR";
-const hejsan = "Use CONST for variables that should not be reassigned";
-console.log(hej);
-console.log(hejsan);
-console.log(hello);
+// var hej = "VAR can have local or global scope";
+// let hello =
+//   "LET can have local, global or block scope. Let is preferred over VAR";
+// const hejsan = "Use CONST for variables that should not be reassigned";
+// console.log(hej);
+// console.log(hejsan);
+// console.log(hello);
+let iBool = true;
+let iString = "a string much wow";
+let iNum = 256;
+console.log(iBool);
+console.log(iString);
+console.log(iNum);
