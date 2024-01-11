33328b9f0a4e749bb3a232107eca03464b09b5bb
mingman <ingman.m@gmail.com>
2024-01-11 12:01:28 +0100

    3.4, 3.5, 3.6, 3.7

--- script3.js
+++ script3.js
@@ -9,3 +9,26 @@ console.log(number2);
 // ---3.3 ---
 let number3 = numbers[3];
 console.log(number3);
+
+// ---3.4 ---
+let secondNumber = numbers[1];
+let fifthNumber = numbers[4];
+let newArray = [secondNumber, fifthNumber];
+console.log(newArray);
+
+// ---3.5 ---
+let lengthNumbers = numbers.length;
+console.log(lengthNumbers);
+
+// ---3.6 ---
+let newVar = numbers.push(12);
+console.log(newVar);
+// array.push() returns new length of array
+
+// ---3.7 ---
+let newVar2 = numbers.unshift(23);
+console.log(newVar);
+// array.unshift() returns new length of array
+let newVar3 = numbers.pop(-1);
+console.log(newVar3);
+//array.pop returns removed value
