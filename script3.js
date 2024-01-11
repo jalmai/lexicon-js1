b8962d276521946d20e712f168c1823aff43ade1
mingman <ingman.m@gmail.com>
2024-01-11 11:51:03 +0100

    3.1, 3.2 and 3.3

---
+++ script3.js
@@ -0,0 +1,11 @@
+// ------ ASSIGNMENT3 - ARRAYS ------
+const numbers = [2, 6, 12, 7, 22, 35];
+// ---3.1 ---
+let number1 = numbers[0];
+console.log(number1);
+// ---3.2 ---
+let number2 = numbers.slice(-1)[0];
+console.log(number2);
+// ---3.3 ---
+let number3 = numbers[3];
+console.log(number3);
