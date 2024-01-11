43f0593a7f6540c872de5fc5f3be53f8e052a924
mingman <ingman.m@gmail.com>
2024-01-11 11:18:01 +0100

    2.3

--- script2.js
+++ script2.js
@@ -1,4 +1,17 @@
 // let arr = [];
 // console.log(arr);
-let arbitraryNumbers = [0, 1, 42, 24601, 1337];
-arbitraryNumbers.forEach((numb) => console.log(numb * 3));
+// let arbitraryNumbers = [0, 1, 42, 24601, 1337];
+// arbitraryNumbers.forEach((numb) => console.log(numb * 3));
+let numbers = [];
+let smallNumbers = [];
+// create array with some numbers
+for (let i = 0; i < 10; i++) {
+  numbers.push(i * 3);
+}
+// Loop through the array of numbers
+numbers.forEach((number) => {
+  if (number < 16) {
+    smallNumbers.push(number);
+  }
+});
+console.log(smallNumbers);
