14c7503777f38d8a036d9aca7773193b1fa84fac
mingman <ingman.m@gmail.com>
2024-01-15 09:17:49 +0100

    adding first functions

---
+++ script6.js
@@ -0,0 +1,11 @@
+function greeting() {
+  alert("Hello there my friend");
+}
+function greetingName(name) {
+  alert("Hello there my friend! You must be" + name + "!");
+}
+function addNumbers(num1, num2) {
+  let sum = num1 + num2;
+  console.log(sum);
+  return sum;
+}
