63f4a2cbe037c7e80ec9d2032254a3f3376a90ed
mingman <ingman.m@gmail.com>
2024-01-18 13:55:11 +0100

    finishing assignment

--- script6.js
+++ script6.js
@@ -4,19 +4,33 @@ let tempWithinRange = false;
 let currentTemp;
 
 while (!tempWithinRange) {
-  currentTemp = farenheitToCelsius(prompt("What temperature is it (F)?"));
+  currentTemp = farenheitToCelsius(prompt("What temperature do you want (F)?"));
   tempCheck();
-  alert(tempWithinRange);
+  if (!tempWithinRange) {
+    alert(
+      "This is not an ideal temperature. Please pick a temperature between " +
+        celsiusToFarenheit(minAcceptableTemp) +
+        " and " +
+        celsiusToFarenheit(maxAcceptableTemp) +
+        " degrees farenheit"
+    );
+  }
 }
 
-function celsiusToFarenheit() {
-  return (currentTemp * 9) / 5 + 32;
+alert(
+  "The temperature is perfect. It is now " +
+    celsiusToFarenheit(currentTemp) +
+    " degrees farenheit in the sauna."
+);
+
+function celsiusToFarenheit(temp) {
+  return (temp * 9) / 5 + 32;
 }
 function farenheitToCelsius(farenheit) {
   return ((farenheit - 32) * 5) / 9;
 }
 function tempCheck() {
-  alert(currentTemp);
+  165;
   if ((currentTemp >= minAcceptableTemp) & (currentTemp <= maxAcceptableTemp)) {
     tempWithinRange = true;
   } else {
